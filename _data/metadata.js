1886687a11f62927f54efca54cf7281d81b64211
Nathan Bottomley <nathan.bottomley@gmail.com>
2025-01-03 21:49:50 +1100

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,12 @@
+export default {
+	title: "Podcaster",
+	subtitle: "eleventy-plugin-podcaster",
+	url: "https://eleventy-plugin-podcaster.com/",
+	language: "en",
+	description: "Documentation website for elevety-plugin-podcaster",
+	author: {
+		name: "Nathan Bottomley",
+		email: "nathan.bottomley@gmail.com",
+		url: "https://gunsandfrocks.com/about/"
+	}
+}
