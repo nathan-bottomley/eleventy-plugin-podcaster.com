657e700bea4f7918fcbd8d331f04e0a22a3e5d5b
Nathan Bottomley <nathan.bottomley@gmail.com>
2025-01-20 07:27:30 +1100

    Beefed up drafts to allow an INCLUDE_DRAFTS env variable to override the setting

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,13 +11,29 @@ import pluginFilters from "./_config/filters.js"
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function(eleventyConfig) {
-	// Drafts, see also _data/eleventyDataSchema.js
-	eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
-		if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
-			return false
-		}
-	})
-
+	let hasLoggedAboutDrafts = false
+  eleventyConfig.addPreprocessor('drafts', 'md', (data, _content) => {
+    let shouldIncludeDrafts = false
+    if (process.env.INCLUDE_DRAFTS === 'true') {
+      shouldIncludeDrafts = true
+    } else if (process.env.INCLUDE_DRAFTS === 'false') {
+      shouldIncludeDrafts = false
+    } else {
+      shouldIncludeDrafts = (process.env.ELEVENTY_RUN_MODE !== 'build')
+    }
+    if (!hasLoggedAboutDrafts) {
+      if (shouldIncludeDrafts) {
+        console.log('Including drafts.')
+      } else {
+        console.log('Excluding drafts.')
+      }
+      hasLoggedAboutDrafts = true
+    }
+    if (data.draft && !shouldIncludeDrafts) {
+      return false
+    }
+  })
+	
 	// Markdown
 
 	const markdownLibrary = MarkdownIt({    
