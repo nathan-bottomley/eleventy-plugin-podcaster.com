cec28f1b2785857bc4a11e80cc8ec48d27ae09c1
Nathan Bottomley <nathan.bottomley@gmail.com>
2026-01-07 22:02:59 +1100

    Fixed javascript formatting

--- content/v1/v1.11tydata.js
+++ content/v1/v1.11tydata.js
@@ -1,10 +1,10 @@
 export default {
-  layout: "layouts/docs.v1.njk",
+  layout: 'layouts/docs.v1.njk',
   permalink (data) {
     if (data.page.fileSlug === 'v1') {
-      return `/v1/docs/`;
+      return '/v1/docs/'
     } else {
-      return `/v1/docs/${data.page.fileSlug}/`;
+      return `/v1/docs/${data.page.fileSlug}/`
     }
   }
 }
