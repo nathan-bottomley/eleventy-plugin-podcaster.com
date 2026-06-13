2ea8655790b8059f926b4cc56614f09570db8411
Nathan Bottomley <nathan.bottomley@gmail.com>
2026-06-13 19:25:22 +1000

    Upgraded format of site.js

--- _data/site.js
+++ _data/site.js
@@ -1,13 +1,13 @@
 export default {
-	title: "Podcaster",
-	subtitle: "eleventy-plugin-podcaster",
-	url: "https://eleventy-plugin-podcaster.com/",
-	language: "en",
-	description: "Documentation website for eleventy-plugin-podcaster.",
-	author: {
-		name: "Nathan Bottomley",
-		email: "nathan.bottomley@gmail.com",
-		url: "https://gunsandfrocks.com/about/"
-	},
-	imageAlt: "A close-up image of a podcasting mic lying on top of some headphones. In the background, out of focus, a keyboard can be seen."
+  title: 'Podcaster',
+  subtitle: 'eleventy-plugin-podcaster',
+  url: 'https://eleventy-plugin-podcaster.com/',
+  language: 'en',
+  description: 'Documentation website for eleventy-plugin-podcaster.',
+  author: {
+    name: 'Nathan Bottomley',
+    email: 'nathan.bottomley@gmail.com',
+    url: 'https://gunsandfrocks.com/about/'
+  },
+  imageAlt: 'A close-up image of a podcasting mic lying on top of some headphones. In the background, out of focus, a keyboard can be seen.'
 }
